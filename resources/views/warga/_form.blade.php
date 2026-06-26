31ef1906596a1388cd0bf28c141fe9428c1010a4
Ahmad Syaifuddin <154029536+ahmad-syaifuddin@users.noreply.github.com>
2026-06-26 12:34:34 +0800

    Update _form.blade.php

--- resources/views/warga/_form.blade.php
+++ resources/views/warga/_form.blade.php
@@ -41,8 +41,8 @@
         <select id="jenis_kelamin" name="jenis_kelamin" required
             class="pl-10 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm transition duration-200">
             <option value="">-- Pilih Jenis Kelamin --</option>
-            <option value="Laki-laki"
-                {{ old('jenis_kelamin', $warga->jenis_kelamin ?? '') == 'Laki-laki' ? 'selected' : '' }}>Laki-laki
+            <option value="Laki-Laki"
+                {{ old('jenis_kelamin', $warga->jenis_kelamin ?? '') == 'Laki-Laki' ? 'selected' : '' }}>Laki-Laki
             </option>
             <option value="Perempuan"
                 {{ old('jenis_kelamin', $warga->jenis_kelamin ?? '') == 'Perempuan' ? 'selected' : '' }}>Perempuan
